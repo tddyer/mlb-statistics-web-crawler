9614ec2568ea969127cdbb8c33bc29e129cee1f1
Tanner <tdyer@hawk.iit.edu>
2020-08-08 13:06:07 -0500

    added data directory for parsed files to appear in

--- mlb_scraper/mlb_scraper/spiders/cubs-spider.py
+++ mlb_scraper/mlb_scraper/spiders/cubs-spider.py
@@ -16,7 +16,7 @@ class CubsSpider(scrapy.Spider):
         filename = '%s.txt' % page
         names = response.css('span.full-3fV3c9pF::text').getall()
         i = 0
-        with open(filename, 'w') as f:
+        with open('../data/%s' % filename, 'w') as f:
             while i < len(names) - 1:
                 f.write(names[i] + ' ' + names[i + 1] + '\n')
                 i += 2
